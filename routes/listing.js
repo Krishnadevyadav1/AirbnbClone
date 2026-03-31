f519c797505bba28fb448615f3cc97ea53dfd2cd
Krishnadevyadav1 <krishna652dev@gmail.com>
2026-03-31 12:17:24 +0530

    fix(listing): normalize empty image url to allow schema default image fallback

---
+++ routes/listing.js
@@ -0,0 +1,86 @@
+const express=require("express");
+const router=express.Router();
+const wrapAsync=require("../utils/wrapAsync.js");
+const ExpressError=require("../utils/ExpressError.js");
+const {listingSchema}=require("../schema.js")
+const  listing =require("../models/listing.js")
+
+
+
+
+const validateListing=(req,res,next)=>{
+     let result=listingSchema.validate(req.body);
+   if(result.error)
+   {
+    let errMsg=result.error.details.map((el)=>el.message).join(",");
+    throw new ExpressError(400,errMsg)
+   }else{
+    next()
+   }
+}
+
+
+
+router.get("/", wrapAsync(async(req,res,next)=>{
+
+    let alllisting=  await listing.find();
+    res.render("listing/index.ejs",{alllisting});
+  
+    
+   
+   
+}))
+router.get("/:id/show", wrapAsync(async(req,res)=>{
+    let {id}=req.params
+   let list=  await listing.findById(id).populate("reviews");
+    res.render("listing/show.ejs",{list});
+    
+}))
+
+router.get("/new", (req,res)=>{
+    
+    
+    res.render("listing/new.ejs")
+})
+router.post("/new", validateListing,wrapAsync(async(req,res)=>{
+    let listing1=new listing(req.body.listing);
+
+
+
+ await listing1.save().then((result)=>{
+    res.redirect("/listing")
+    
+})}))
+
+
+
+router.get("/:id/edit", wrapAsync(async (req,res)=>{
+    let {id}=req.params
+    
+    
+   let list=  await listing.findById(id);
+    res.render("listing/edit.ejs",{list});
+    
+}))
+
+router.put("/:id/edit",validateListing,wrapAsync(async (req,res)=>{
+   
+     let {id}=req.params;
+ 
+
+     await listing.findByIdAndUpdate(id,{...req.body.listing})
+     
+     res.redirect(`/listing/${id}/show`)
+
+    
+}))
+router.delete("/:id/delete",wrapAsync(async(req,res)=>{
+ let {id}=req.params;
+
+ let del= await listing.findByIdAndDelete(id);
+res.redirect("/listing")
+console.log(del)
+
+}))
+
+module.exports=router
--- schema.js
+++ schema.js
@@ -1,19 +1,26 @@
 const joi =require("joi")
-module.exports.listingschema=joi.object({
+module.exports.listingSchema=joi.object({
     listing:joi.object({
         title:joi.string().required(),
         description:joi.string().required(),
         country:joi.string().required(),
         location:joi.string().required(),
         price:joi.number().required().min(0),
-        image:joi.string().allow("",null)
+        image:joi.object({
+            filename:joi.string().allow("", null),
+            url:joi.string().allow("",null)
+        })
         
     }).required()
 })
 
-module.exports.reviewschema=joi.object({
+module.exports.reviewSchema=joi.object({
     reviews:joi.object({
         rating:joi.number().required().min(1).max(5),
         comment:joi.string().required(),
     }).required(),
 })
+
+// Backward-compatible aliases
+module.exports.listingschema = module.exports.listingSchema;
+module.exports.reviewschema = module.exports.reviewSchema;
