4b33988013a6e6908cccfa3820ae115185d43e03
Krishnadevyadav1 <krishna652dev@gmail.com>
2026-04-02 23:21:29 +0530

    chore(app): wire auth routes and middleware

--- app.js
+++ app.js
@@ -4,19 +4,21 @@ const path=require("path");
 const mongoose=require("mongoose");
 const session = require('express-session')
 const flash=require("connect-flash")
-
 const ejsmate=require("ejs-mate")
-
 const ExpressError=require("./utils/ExpressError.js");
-
-
 const listings=require("./routes/listing.js")
 const review=require("./routes/review.js");
+const passport=require("passport")
+const LocalStrategy=require("passport-local")
+const User=require("./models/user.js")
+const user=require("./routes/user.js")
+
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 const methodOverride=require("method-override");
+
 
 app.use(methodOverride("_method"));
 app.engine("ejs",ejsmate);
@@ -56,16 +58,33 @@ app.get("/",(req,res)=>{
 app.use(session(sessionOptions));
 app.use(flash())
 
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next)=>{
     res.locals.success=req.flash("success");
     res.locals.error=req.flash("error");
     next();
 })
 
+// app.get("/demouser",async(req,res)=>{
+//     let fakeuser=new User({
+//         email:"kdy@gmail.com",
+//         username:"kdy"
+//     })
+//    const ruser=await  User.register(fakeuser,"password")
+//    res.send(ruser)
+// })
+
 
 app.use("/listing",listings)
 
 app.use("/listing/:id/reviews",review)
+
+app.use("/",user)
 
 
 // Catch-all route
