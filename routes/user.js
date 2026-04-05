88ca8c3b2f158d25ce575545897bc095fa0044a7
Krishnadevyadav1 <krishna652dev@gmail.com>
2026-04-05 11:46:55 +0530

    feat(auth): update user routes and navbar auth UI

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,8 @@ const router=express.Router();
 const ExpressError=require("../utils/ExpressError.js");
 const User=require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
-const passport=require("passport")
+const passport=require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
 
 
@@ -15,10 +16,18 @@ router.post("/signup",wrapAsync(async(req,res)=>{
     
     try{const{username,email,password }=req.body;
     const nuser= new User({email,username})
-    await User.register(nuser,password)
-    req.flash("success","Registration Successful!")
+  const reguser=  await User.register(nuser,password)
+   req.login(reguser,(err)=>{
+        if(err)
+        {
+            next(err)
+        }
+         req.flash("success","Registration Successful!")
    
-    res.redirect("/listing")}
+    res.redirect("/listing")
+    })
+   
+   }
 
     catch(err)
     {
@@ -32,10 +41,23 @@ router.post("/signup",wrapAsync(async(req,res)=>{
 router.get("/login",(req,res)=>{
 res.render("users/login.ejs")
 })
-router.post("/login", passport.authenticate("local",{failureRedirect:'/login',failureFlash:true}),async(req,res)=>
-{
-    res.send("login successful")
+router.post("/login",saveRedirectUrl, passport.authenticate("local",{failureRedirect:'/login',failureFlash:true}),async(req,res)=>
+{   
+    req.flash("success","login successful")
+    let rdu=res.locals.redirectUrl||"/listing"
+    res.redirect(rdu)
 
+})
+
+router.get("/logout",(req,res,next)=>{
+    req.logout((err)=>{
+        if(err)
+        {
+            next(err);
+        }
+        req.flash("success","logout successful")
+        res.redirect("/listing")
+    })
 })
 
 module.exports=router
